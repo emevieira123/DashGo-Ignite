aeb06912bf2c4d5e5573847df3c9ca6f3e6d2c15
Emerson Vieira <33058881+emevieira123@users.noreply.github.com>
2021-12-28 21:05:16 -0400

    Deixando a aplicacao responsiva

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex>
-            <Box mr="4" textAlign="right">
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
                 <Text>Emerson Vieira</Text>
                 <Text color="gray.300" fontSize="smal">
                     emevieira@gmail.com
                 </Text>
             </Box>
+            )}
 
             <Avatar size="md" name="Emerson Vieira" src="https://github.com/emevieira123.png" />
         </Flex>
